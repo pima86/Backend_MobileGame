544dba798594e42dfc40d04efbbf1e0dbc4ede24
pima86 <71416955+pima86@users.noreply.github.com>
2023-02-20 01:54:27 +0900

    Add files via upload

---
+++ CameraMovement.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    [SerializeField] private Camera cam;
+    [SerializeField] private float zoomStep, minCamSize, maxCamSize;
+    [SerializeField] private SpriteRenderer mapRenderer;
+
+    private Vector3 dragOrigin;
+    private float mapMinX, mapMaxX, mapMinY, mapMaxY;
+
+    private void Awake()
+    {
+        mapMinX = mapRenderer.transform.position.x - mapRenderer.bounds.size.x / 2f;
+        mapMaxX = mapRenderer.transform.position.x + mapRenderer.bounds.size.x / 2f;
+
+        mapMinY = mapRenderer.transform.position.y - mapRenderer.bounds.size.y / 2f;
+        mapMaxY = mapRenderer.transform.position.y + mapRenderer.bounds.size.y / 2f;
+    }
+
+    float zoom_timer = 0;
+    private void Update()
+    {
+        if (Input.touchCount == 2)
+            TouchZoom();
+        else
+            PanCamera();
+    }
+
+    private void PanCamera()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            dragOrigin = cam.ScreenToWorldPoint(Input.mousePosition);
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 difference = dragOrigin - cam.ScreenToWorldPoint(Input.mousePosition);
+
+            cam.transform.position = ClampCamera(cam.transform.position + difference);
+        }
+    }
+
+    void TouchZoom()
+    {
+        Touch touchZero = Input.GetTouch(0); //첫번째 손가락 터치를 저장
+        Touch touchOne = Input.GetTouch(1); //두번째 손가락 터치를 저장
+
+        //터치에 대한 이전 위치값을 각각 저장함
+        //처음 터치한 위치(touchZero.position)에서 이전 프레임에서의 터치 위치와 이번 프로임에서 터치 위치의 차이를 뺌
+        Vector2 touchZeroPrevPos = touchZero.position - touchZero.deltaPosition; //deltaPosition는 이동방향 추적할 때 사용
+        Vector2 touchOnePrevPos = touchOne.position - touchOne.deltaPosition;
+
+        // 각 프레임에서 터치 사이의 벡터 거리 구함
+        float prevTouchDeltaMag = (touchZeroPrevPos - touchOnePrevPos).magnitude; //magnitude는 두 점간의 거리 비교(벡터)
+        float touchDeltaMag = (touchZero.position - touchOne.position).magnitude;
+
+        // 거리 차이 구함(거리가 이전보다 크면(마이너스가 나오면)손가락을 벌린 상태_줌인 상태)
+        float deltaMagnitudeDiff = prevTouchDeltaMag - touchDeltaMag;
+
+        float newSize = cam.orthographicSize + (deltaMagnitudeDiff * zoomStep);
+        cam.orthographicSize = Mathf.Clamp(newSize, minCamSize, maxCamSize);
+        cam.transform.position = ClampCamera(cam.transform.position);
+    }
+
+    private Vector3 ClampCamera(Vector3 targetPosition)
+    {
+        float camHeight = cam.orthographicSize;
+        float camWidth = cam.orthographicSize * cam.aspect;
+
+        float minX = mapMinX + camWidth;
+        float maxX = mapMaxX - camWidth;
+        float minY = mapMinY + camHeight;
+        float maxY = mapMaxY - camHeight;
+
+        float newX = Mathf.Clamp(targetPosition.x, minX, maxX);
+        float newY = Mathf.Clamp(targetPosition.y, minY, maxY);
+
+        return new Vector3(newX, newY, targetPosition.z);
+    }
+}
